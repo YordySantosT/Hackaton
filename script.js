e2b1d479e947a23c2df782b8bbbbde01d4273e38
ArturoRuge <arturoruge@gmail.com>
2024-09-16 16:44:36 -0500

    add content in HTML

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const MiBoton = document.getElementById("mibotn");
+const botones = document.querySelectorAll(".miBoton")
 const carrito = document.getElementById("carrito");
 const eliminar = document.getElementById("eliminarbtn");
 
@@ -6,17 +6,44 @@ const eliminar = document.getElementById("eliminarbtn");
 const modalElement = document.getElementById('miModal');
 const modal = new bootstrap.Modal(modalElement);
 
-MiBoton.addEventListener("click", function(){
-    const nuevoProducto = document.createElement("img");
-    nuevoProducto.src = "https://www.shutterstock.com/image-vector/goldfish-isolated-on-white-background-260nw-2479774033.jpg";
 
-    carrito.appendChild(nuevoProducto);
+
+// funcion que por cada boton del producto añade la img y titulo al carrito
+botones.forEach(boton => {
+    boton.addEventListener("click",function(){
+
+
+        //traigo el numero del producto quitando boton y solo dejando el numero y traigo el titulo acorde
+        const imgNumber = boton.id.replace("boton", "");
+        const titulo = document.getElementById(`titulo${imgNumber}`);
+
+        // creo el img y h5
+        const nuevoProducto = document.createElement("img");
+        const nuevoTitulo = document.createElement("h5");
+
+        //especifico que la imgen y titulo deben de ser correspondientes al numero
+        nuevoProducto.src = `img/img${imgNumber}.jpg`;
+        nuevoTitulo.textContent = titulo.textContent;
+
+        //añado un espacio despues de cada img y titulo
+        const espacio = document.createElement("br");
+
+        //enviar todo para que se vea
+        carrito.appendChild(nuevoProducto);
+        carrito.appendChild(nuevoTitulo);
+        carrito.appendChild(espacio);
+    });
 });
 
+
+// Esta funcion elimina 3 ultimo elementos (img,br,titulo)
 eliminar.addEventListener("click", function(){
     if(carrito.hasChildNodes()){
         carrito.removeChild(carrito.lastChild);
+        carrito.removeChild(carrito.lastChild);
+        carrito.removeChild(carrito.lastChild);
+        
     }else{
         modal.show();
     }
-})
+});
